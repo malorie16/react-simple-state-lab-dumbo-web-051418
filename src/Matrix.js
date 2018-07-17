dd2a001e15bd963438e633bd8742f52ff27209fc
Malorie <malorie18@gmail.com>
2018-07-16 22:45:56 -0400

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
+import Cell from './cell.js'
 
 export default class Matrix extends Component {
-  
+  // constructor(props) {
+  //   super(props)
+  //   jimmy ||= shimmy
+  //   jimmy = jimmy || shimmy
+  //
+  //
+  // }
+
   genRow = (vals) => (
-    vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
+    vals.map(val => <Cell values={val}/>) // replace me and render a cell component instead!
   )
-  
+
   genMatrix = () => (
     this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
   )
-  
+
   render() {
     return (
       <div id="matrix">
@@ -17,5 +25,21 @@ export default class Matrix extends Component {
       </div>
     )
   }
-  
+
+}
+
+Matrix.defaultProps = {
+  values: (() => {
+    return
+    (['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+    ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'])
+  })
 }
---
+++ src/cell.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+export default class Cell extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      color: this.props.values
+    }
+  }
+
+  handleClick = (event) => {
+    this.setState({
+      color: '#333'
+    })
+  }
+
+
+  render() {
+    return (
+      <div className='cell' style={{backgroundColor: this.state.color}} onClick={this.handleClick}></div>
+    )
+  }
+}
